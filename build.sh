14eb36eb769c99b3ac9a7c940465bd54a4cc48b5
Bellardia <ian@elliian.com>
2016-08-22 16:41:41 -0400

    Further bug fixes on build script

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 sudo apt-get update
 sudo apt-get install -y build-essential zlib1g-dev libpcre3 libpcre3-dev unzip
 
-wget https://github.com/Jumbleberry/NginxSwarm/blob/master/zlib-1.2.8.tar.gz?raw=true
+wget https://github.com/Jumbleberry/NginxSwarm/blob/master/pcre-8.39.tar.gz?raw=true -O pcre-8.39.tar.gz
 tar -zxf pcre-8.39.tar.gz
 cd pcre-8.39
 ./configure
@@ -9,7 +9,8 @@ make
 sudo make install
 cd ../
 
-wget https://github.com/Jumbleberry/NginxSwarm/blob/master/pcre-8.39.tar.gz?raw=true
+
+wget https://github.com/Jumbleberry/NginxSwarm/blob/master/zlib-1.2.8.tar.gz?raw=true -O zlib-1.2.8.tar.gz
 tar -zxf zlib-1.2.8.tar.gz
 cd zlib-1.2.8
 ./configure
@@ -17,7 +18,7 @@ make
 sudo make install
 cd ../
 
-wget https://github.com/Jumbleberry/NginxSwarm/blob/master/openssl-1.0.2h.tar.gz?raw=true
+wget https://github.com/Jumbleberry/NginxSwarm/blob/master/openssl-1.0.2h.tar.gz?raw=true -O openssl-1.0.2h.tar.gz
 tar -zxf openssl-1.0.2h.tar.gz
 cd openssl-1.0.2h
 patch -d . -p 1 < ../nginx_chacha.patch
@@ -26,15 +27,14 @@ make
 sudo make install
 cd ../
 
-NPS_VERSION=1.11.33.3
-wget https://github.com/Jumbleberry/NginxSwarm/blob/master/release-1.11.33.3-beta.zip?raw=true
-unzip release-${NPS_VERSION}-beta.zip
-cd ngx_pagespeed-release-${NPS_VERSION}-beta/
-wget https://github.com/Jumbleberry/NginxSwarm/blob/master/1.11.33.3.tar.gz?raw=true
-tar -xzvf ${NPS_VERSION}.tar.gz  # extracts to psol/
+wget https://github.com/Jumbleberry/NginxSwarm/blob/master/release-1.11.33.3-beta.zip?raw=true -O release-1.11.33.3-beta.zip
+unzip release-1.11.33.3-beta.zip
+cd ngx_pagespeed-release-1.11.33.3-beta/
+wget https://github.com/Jumbleberry/NginxSwarm/blob/master/1.11.33.3.tar.gz?raw=true -O 1.11.33.3.tar.gz
+tar -xzvf 1.11.33.3.tar.gz  # extracts to psol/
 cd ../
 
-wget https://github.com/Jumbleberry/NginxSwarm/blob/master/nginx-1.9.7.tar.gz?raw=true
+wget https://github.com/Jumbleberry/NginxSwarm/blob/master/nginx-1.9.7.tar.gz?raw=true -O nginx-1.9.7.tar.gz
 tar -zxf nginx-1.9.7.tar.gz
 CURPATH=$(readlink -f . | sed -r 's/^.{1}//');
 cd nginx-1.9.7/
@@ -55,7 +55,7 @@ patch -d . -p 1 < ../nginx_http2_spdy.patch
 --with-openssl=/${CURPATH}/openssl-1.0.2h \
 --user=www-data \
 --group=www-data \
---add-module=/${CURPATH}/ngx_pagespeed-release-${NPS_VERSION}-beta ${PS_NGX_EXTRA_FLAGS} 
+--add-module=/${CURPATH}/ngx_pagespeed-release-1.11.33.3-beta ${PS_NGX_EXTRA_FLAGS} 
 make
 sudo make install
 cd ../
